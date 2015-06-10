168c0faeb3c4d45f68c347d29756508006d73151
Ravinder Singh <rav91singh@gmail.com>
2015-06-11 00:29:41 +0530

    commit #15 itinerary page layout beta complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+current = 1;
+function toggleItineraryPanels(data){
+	if(data == current){
+		$("#collapse"+data).collapse("toggle");
+		return false;
+	}else{
+		$("#collapse"+data).collapse("toggle");
+		$("#collapse"+current).collapse("toggle");
+		current = data;					
+		
+	}
+}
+
 function lookUpItinerary(){
 	var target = "goa";
 	var jsonresponse = '{"placesToVisit":{"Panaji":{"popularityQuotient":"1","image":{"carousel":[{"img":"image1.jpg","desc":"image brief(30 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"image brief(30 words max) to accompany the carousel image2.jpg"}]},"thumbs":[{"img":"image1.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image2.jpg"}],"desc":{"mini":"This is the panaji one - liner","long":"This is a relatively longer description of Panaji.Can contain multiple Paragraphs. "}},"Anjuna":{"popularityQuotient":"3","image":{"carousel":[{"img":"image1.jpg","desc":"image brief(30 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"image brief(30 words max) to accompany the carousel image2.jpg"}]},"thumbs":[{"img":"image1.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image2.jpg"}],"desc":{"mini":"This is the panaji one - liner","long":"This is a relatively longer description of Panaji.Can contain multiple Paragraphs. "}},"Cola":{"popularityQuotient":"2","image":{"carousel":[{"img":"image1.jpg","desc":"image brief(30 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"image brief(30 words max) to accompany the carousel image2.jpg"}]},"thumbs":[{"img":"image1.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image2.jpg"}],"desc":{"mini":"This is the panaji one - liner","long":"This is a relatively longer description of Panaji.Can contain multiple Paragraphs. "}},"Dhoodhsagar":{"popularityQuotient":"4","image":{"carousel":[{"img":"image1.jpg","desc":"image brief(30 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"image brief(30 words max) to accompany the carousel image2.jpg"}]},"thumbs":[{"img":"image1.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image2.jpg"}],"desc":{"mini":"This is the panaji one - liner","long":"This is a relatively longer description of Panaji.Can contain multiple Paragraphs. "}},"Bagha":{"popularityQuotient":"3","image":{"carousel":[{"img":"image1.jpg","desc":"image brief(30 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"image brief(30 words max) to accompany the carousel image2.jpg"}]},"thumbs":[{"img":"image1.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image1.jpg"},{"img":"image2.jpg","desc":"Short one-liner(10 words max) to accompany the carousel image2.jpg"}],"desc":{"mini":"This is the panaji one - liner","long":"This is a relatively longer description of Panaji.Can contain multiple Paragraphs. "}}},"itineryData":{"id":"GA_IT_001","location_id":"GA_IND","it_data":{"days":"4","specifics":{"1":{"location":"Arrival","desc":"Arrive at APT,Move to Hotel,Evening Free"},"2":{"location":"Panaji","desc":"Fuck around through out the day in Panaji"},"3":{"location":"Bagha","desc":"Roam around for a chance to fuck a white chick and be one-shade darker by the day-end"},"4":{"location":"Departure","desc":"No gain all in vain."}}}},"check":"success"}';
@@ -40,7 +53,7 @@ function displayItinerary(data){
 		var descript = it_specifics[key].desc;
 		itineraryHolderHtml 	+= 	'	<div class="panel panel-default">';
 		itineraryHolderHtml		+=	'		<div class="panel-heading">';
-		itineraryHolderHtml		+=	'			<a href="#collapse'+key+'" data-toggle="collapse" data-parents="asda">Day '+key+'</a>';
+		itineraryHolderHtml		+=	'			<a href="javascript:void(0);" onclick="javascript:toggleItineraryPanels('+key+');">Day '+key+'</a>';
 		itineraryHolderHtml		+=	'		</div>';
 		itineraryHolderHtml		+=	'	</div>';
 
@@ -60,7 +73,26 @@ function displayItinerary(data){
 		itineraryHolderHtml		+= '					<p>'+descript+'</p>';
 		itineraryHolderHtml		+= '				</div>';
 		itineraryHolderHtml		+= '				<div class="col-sm-4">';
-		itineraryHolderHtml		+= '					<img src="images/goa-beach.jpg" class="img-circle" alt="Cinque Terre" width="120" height="100">';
+		itineraryHolderHtml		+= '					<img src="images/goa-beach.jpg" class="img-circle" alt="Cinque Terre" width="120" height="100" onclick="javascript:showOverLay();">';
+		itineraryHolderHtml		+= '				</div>';
+		itineraryHolderHtml		+= '			</div>';
+		itineraryHolderHtml		+= '			<div class="row" >';
+		itineraryHolderHtml		+= '				<div class="pull-left" style="margin-left:10px;">';
+		if(key >1 && key<= days){
+			var temp = parseInt(key)-1;
+			itineraryHolderHtml		+= '				<a href="javascript:void(0);" onclick="javascript:toggleItineraryPanels('+temp+');"><i class="fa fa-angle-double-left"></i></a>';
+
+		}
+
+		itineraryHolderHtml		+= '				</div>';
+		itineraryHolderHtml		+= '				<div class="pull-right" style="margin-right:10px;">';
+		if(key < days){
+			var temp = parseInt(key)+1;
+			itineraryHolderHtml		+= '				<a href="javascript:void(0);" onclick="javascript:toggleItineraryPanels('+temp+');"><i class="fa fa-angle-double-right"></i></a>';
+
+		}else{
+			itineraryHolderHtml		+= '				<a href="javascript:void(0);" onclick="javascript:callFinishTask();">Finish</a>';			
+		}
 		itineraryHolderHtml		+= '				</div>';
 		itineraryHolderHtml		+= '			</div>';
 		itineraryHolderHtml		+= '		</div>';
@@ -69,4 +101,14 @@ function displayItinerary(data){
 	$("#itineraryHolderDiv").html(itineraryHolderHtml);
 
 
+}
+
+function showOverLay(){
+	var width = ($(".fullScreenOverlay").outerWidth()/2) - ($(".charC").outerWidth()/2);
+	var height = ($(".fullScreenOverlay").outerHeight()/2) - ($(".charC").outerHeight()/2);
+	$(".charC").css({"left":width,"top":height});
+	$(".fullScreenOverlay").show();
+}
+function callFinishTask(){
+	alert("Finish");
 }
